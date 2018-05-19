ba146fecec662bd20a4bfd56c89c3e8906cf41fb
Alice <a.j.stack@sbcglobal.net>
2018-05-19 13:40:16 -0500

    added ticket master ajax

--- logic.js
+++ logic.js
@@ -57,3 +57,24 @@ $.ajax({
     youTube = response
    console.log(youTube);
   });
+
+
+  var apiKey = "kJgzQXUSlb55GtDLH5Qh4BI1eZYNZvcp";
+  var bandName = "" //$(this).attr("data-topic"); {put this sort of thing here}
+  
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&keyword="+ bandName + "&apikey=" + apiKey;
+  
+  $.ajax({
+    type:"GET",
+    url:queryURL,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                // Parse the response.
+                // Do other things.
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  });
