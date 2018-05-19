84703aa10531cedfd007a4e1ae14237a7e55e33d
Alan <andrewsalan88@gmail.com>
2018-05-19 11:24:27 -0500

    got wikipedia to respond

--- logic.js
+++ logic.js
@@ -1,6 +1,49 @@
-$.ajax({
-    url: "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8eb091be8b1dd1a4700dc3b2a357bc1d",
-    method: "GET"
+// console.log("hi");
+// var itunesInfo;
+// $.ajax({
+//     url: "https://itunes.apple.com/search?term=Whitney&limit=5",
+//     method: "GET"
+//   }).then(function(response) {
+//     itunesInfo = response
+//     console.log(itunesInfo);
+//   });
+
+  
+  // console.log("hi");
+  // var itunesInfo;
+  // $.ajax({
+  //     url: "https://itunes.apple.com/search?term=Whitney&limit=5",
+  //     method: "GET"
+  //   }).then(function(response) {
+  //     itunesInfo = response
+  //     console.log(itunesInfo);
+  //   });
+  
+    
+
+  // Using jQuery
+  var wikiData;
+$.ajax( {
+  url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
+  // data: queryData,
+  dataType: 'jsonp',
+  type: 'POST',
+  headers: { 'Api-User-Agent': 'Example/1.0' },
+  // success: function(data) {
+  //    // do something with data
+  // }
   }).then(function(response) {
-    console.log(response);
+    wikiData = response;
+    console.log(wikiData);
   });
+
+
+
+// var wikiData;
+// $.ajax({
+//       url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
+//       method: "GET"
+//     }).then(function(response) {
+//       wikiData = response
+//       console.log(wikiData);
+//     });
