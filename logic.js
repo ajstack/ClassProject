a373d589d3835fa1c344c5184cc50d258968b580
Alice <a.j.stack@sbcglobal.net>
2018-05-19 13:00:21 -0500

    I don't actually want to push any of this...

--- logic.js
+++ logic.js
@@ -1,6 +1,19 @@
+var apiKey = "kJgzQXUSlb55GtDLH5Qh4BI1eZYNZvcp";
+var bandName = "" //$(this).attr("data-topic"); {put this sort of thing here}
+
+var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&keyword="+ bandName + "&apikey=" + apiKey;
+
 $.ajax({
-    url: "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8eb091be8b1dd1a4700dc3b2a357bc1d",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+  type:"GET",
+  url:queryURL,
+  async:true,
+  dataType: "json",
+  success: function(json) {
+              console.log(json);
+              // Parse the response.
+              // Do other things.
+           },
+  error: function(xhr, status, err) {
+              // This time, we do not end up here!
+           }
+});
