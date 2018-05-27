099dfd15eba22add03f173c030b8c8a1d72f70bf
Alan <andrewsalan88@gmail.com>
2018-05-27 09:50:28 -0500

    making my branch match the master branch

--- logic.js
+++ logic.js
@@ -1,66 +1,5 @@
-// console.log("hi");
-
-// var itunesInfo;
-// var artist;
-// var itunesQueryURL = "https://itunes.apple.com/search?term=" + artist + "&limit=5";
-
-
-$("#artistSearch").on("click", function(event){
-    event.preventDefault();
-    // console.log("search");
-  
-    var artist = $("#artist").val().trim();
-    // console.log(artist);
-  
-    var itunesQueryURL = "https://itunes.apple.com/search?term="+ artist + "&limit=5";
-
-$.ajax({
-  url: itunesQueryURL,
-  method: "GET"
-}).then(function(response) {
-    itunesInfo = response
-  //console.log(itunesInfo);
-  var itunesObject = JSON.parse(itunesInfo);
-  // console.log(itunesObject);
-  
-  var itunesResults = itunesObject.results
-  // console.log(itunesResults);
-
-  for (var i=0; i<itunesResults.length; i++){
-    console.log(itunesResults[i].artistName);
-    console.log(itunesResults[i].trackName);
-    console.log(itunesResults[i].artistViewUrl);
-    console.log(itunesResults[i].trackViewUrl);
-  }
-});
-} );
-
-
-// }
-//   var itunesObject = JSON.parse(itunesInfo);
-//   console.log(itunesObject);
-
-
-// var itunesInfo;
-// $.ajax({
-//     url: "https://itunes.apple.com/search?term=Whitney&limit=5",
-//     method: "GET"
-//   }).then(function(response) {
-//     itunesInfo = response
-//     console.log(itunesInfo);
-//   });
-
-
 
 // Using jQuery
-// $("#artistSearch").on("click", function(event){
-//   event.preventDefault();
-
-//   var artist = $("#artist").val().trim();
-//   var wikipedia
- 
-//   var wikipediaURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + artist + "&key=" + youTubeAPIkey;
-
 // var wikiData;
 // $.ajax( {
 // url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
@@ -75,7 +14,6 @@ $.ajax({
 //   wikiData = response;
 //   console.log(wikiData);
 
-// });
 
 // });
 
@@ -99,11 +37,12 @@ $("#artistSearch").on("click", function (event) {
   var artist = $("#artist").val().trim();
   var youTube
   var youTubeAPIkey = "AIzaSyDXIkTs44eGNfH2r9jIyECiQgv4dJ6_RWM";
-  var youTubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + artist + "&key=" + youTubeAPIkey;
+  var youTubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + artist + "&key=" + youTubeAPIkey;
   $.ajax({
     // url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=surfing&key=AIzaSyDXIkTs44eGNfH2r9jIyECiQgv4dJ6_RWM",
     url: youTubeURL,
-    method: "GET"
+    method: "GET",
+    dataType: 'jsonp' // added this in hopes of stopping the MIME error, it didn't work
   }).then(function (response) {
     youTube = response
     // console.log(youTube);
@@ -112,12 +51,34 @@ $("#artistSearch").on("click", function (event) {
     for (var i = 0; i < youTubeVideos.length; i++) {
       //console.log(youTubeVideos[i].snippet.title);
       //console.log("https://www.youtube.com/watch?v=" + youTubeVideos[i].id.videoId);
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement("script");
 
+      tag.src = "https://www.youtube.com/watch?v=" + youTubeVideos[i].id.videoId;
+      var firstScriptTag = document.getElementsByTagName("script")[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player("player", {
+          height: "390",
+          width: "640",
+          videoId: youTubeVideos[i].id.videoId,
+          events: {
+            "onReady": onPlayerReady,
+            "onStateChange": onPlayerStateChange
+          }
+        });
+      }
     }
   });
   // var youTubeVidEmbed = youTube.items.
 
-// });
+
+
+});
 
 
 // $(document).on("click", ".tunes", displayItunesInfo);
@@ -126,55 +87,9 @@ $("#artistSearch").on("click", function (event) {
   event.preventDefault();
   // console.log("search");
 
+  var artist = $("#artist").val().trim();
+  // console.log(artist);
 
-<<<<<<< HEAD
-
-// $("#artistSearch").on("click", function(event){
-//   event.preventDefault();
-//   // console.log("search");
-
-//   var artist = $("#artist").val().trim();
-//   // console.log(artist);
-
-//   var apiKey = "kJgzQXUSlb55GtDLH5Qh4BI1eZYNZvcp";
- 
-  
-//   var ticketMasterQueryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=3&keyword="+ artist + "&apikey=" + apiKey;
-  
-//   $.ajax({
-//     type:"GET",
-//     url:ticketMasterQueryURL,
-//     async:true,
-//     dataType: "json",
-//     success: function(ticketMasterResults) {
-//                     //  console.log(ticketMasterResults);
-//                      var ticketMasterEvents = ticketMasterResults._embedded.events;
-//                     //  console.log(ticketMasterEvents);
-//                      // Parse the response.
-//                      // Do other things.
-//                       for (var i=0; i<ticketMasterEvents.length; i++){
-//                         // console.log(ticketMasterEvents[i].name);
-//                         // console.log(ticketMasterEvents[i].url);
-//                         var listItem = $("<li>");
-//                         var tourDates = ("<a>" + ticketMasterEvents[i].name + "</a>");
-//                         $(tourDates).attr("href", ticketMasterEvents[i].url);
-//                         listItem.append(tourDates);
-
-//                         $(tourDates).appendTo("#tour-dates");
-                        
-//                       }
-
-
-
-
-//                   },
-//     error: function(xhr, status, err) {
-//                 // This time, we do not end up here!
-//              }
-//   });
-
-//     } );
-=======
   var ticketMasterApiKey = "kJgzQXUSlb55GtDLH5Qh4BI1eZYNZvcp";
 
 
@@ -208,10 +123,6 @@ $("#artistSearch").on("click", function (event) {
     }
   });
 
-<<<<<<< HEAD
-
-    } );
-=======
   var itunesQueryURL = "https://itunes.apple.com/search?term=" + artist + "&limit=3";
 
   $.ajax({
@@ -225,25 +136,26 @@ $("#artistSearch").on("click", function (event) {
 
     var itunesResults = itunesObject.results
     console.log(itunesResults);
-
+    //$("#").empty(); {put div for itunes here to clear with every search}
+    $("#artist-image").empty();
     for (var i = 0; i < itunesResults.length; i++) {
       console.log(itunesResults[i].artistName);
       console.log(itunesResults[i].trackName);
       console.log(itunesResults[i].artistViewUrl);
       console.log(itunesResults[i].trackViewUrl);
 
-      var artistImg = $("<img>");
       
-      artistImg.attr("src", itunesResults[i].artworkUrl100);
-
-      $(artistImg).appendTo("#images");
     }
+    var artistImg = $("<img>");
+
+    artistImg.attr("src", itunesResults[0].artworkUrl100);
+
+    $(artistImg).appendTo("#artist-image");
+    //$(artistImg).appendTo("#wikipedia");
   });
 
 
 });
 
->>>>>>> master
->>>>>>> master
 
 
